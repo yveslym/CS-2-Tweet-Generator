30f662eaa92a3bed65b0aad6e639d0dbfc91139b
yves songolo <yves.songolo@gmail.com>
2017-10-26 16:09:41 -0700

    some change

---
+++ random_dictionary_word.py
@@ -0,0 +1,12 @@
+import random
+import sys
+
+
+def generate_word(number):
+    let path = "wc /usr/share/dict/words"
+    index = 0
+    while index < number:
+        
+
+
+if __name__=="__main__":

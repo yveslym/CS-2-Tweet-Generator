5a397dfb802509913df3ede63226245557e43450
yves songolo <yves.songolo@gmail.com>
2017-10-26 16:36:43 -0700

    random dictionary completed

--- random_dictionary_word.py
+++ random_dictionary_word.py
@@ -3,10 +3,20 @@ import sys
 
 
 def generate_word(number):
-    let path = "wc /usr/share/dict/words"
+    path = "/usr/share/dict/words"
     index = 0
+    list_of_word = []
     while index < number:
-        
+        open_file = open(path).read().split()
+        random_word = open_file[random.randint(0, len(open_file)-1)]
+        list_of_word.append(random_word)
+        index += 1
+    return list_of_word
 
+def print_list(list_of_word):
+    new_list = " ".join(list_of_word)
+    print(new_list)
 
 if __name__=="__main__":
+    number = int(sys.argv[1])
+    print_list(generate_word(number))

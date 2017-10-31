dc0a5d5fbd974fd75dd7bea3342b9414d7e267a7
yves songolo <yves.songolo@gmail.com>
2017-10-30 23:34:05 -0700

    complet histogram

--- histogram.py
+++ histogram.py
@@ -6,8 +6,28 @@ def histogram(source_text):
 
     open_files = ('/Users/yveslym/Documents/CS-2-Tweet-Generator/%s' % source_text)
     list_of_word = open(open_files).read().split()
+    print('number if element in list_of_word: %s' % len(list_of_word))
+    return list_of_word
 
-    unique_list = list(set(list_of_word))
+
+def unique_word(list_of_word):
+    unique_list = ['']#list(set(list_of_word))
+    count = 0
+    print("while looping #%s" % count)
+    for word in list_of_word:
+        print("for looping #%s" % count)
+        count +=1
+        index = 0
+        uniques = True
+        while index < len(unique_list):
+            if word == unique_list[index]:
+                uniques = False
+            #print("while looping #%s" % index)
+            index += 1
+        if uniques == True:
+            unique_list.append(word)
+
+
     print('number if element in list_of_word: %s' % len(list_of_word))
 
     print('number if element in unique_list: %s' % len(unique_list))
@@ -24,4 +44,4 @@ def histogram(source_text):
 
 
 if __name__ == "__main__":
-    histogram('Letter-That-Have-Helped-Me-by-Various.txt')
+    unique_word(histogram('Letter-That-Have-Helped-Me-by-Various.txt'))

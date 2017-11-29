3846d0771261562cdfb0dbbffab42d755045b283
yves songolo <yves.songolo@gmail.com>
2017-11-28 21:01:05 -0800

    Double linked list completed

--- doubleLinkedListTest.py
+++ doubleLinkedListTest.py
@@ -31,6 +31,108 @@ class doubleLinkedList(unittest.TestCase):
         assert dll.tail.next is None
         assert dll.head == dll.head.next.prev
         assert dll.tail == dll.tail.prev.next
+    
+    def test_prepend(self):
+        head = None
+        tail = None
+
+        for item in array:
+            new_node = Node(item)
+            if head is None:
+                head = new_node
+                tail = new_node
+            else:
+                head.prev = new_node
+                new_node.next = head
+                head = new_node
+        assert head.data == 6
+        assert tail.data == 1
+        assert head.prev is None
+        assert tail.next is None
+        assert head.next.prev == head
+        assert tail.prev.next == tail
+    
+    def test_prepend_implementation(self):
+        dll = DoubleLinkedList()
+        assert dll.prepend(1) == 1
+        assert dll.prepend(2) == 2
+        assert dll.prepend(3) == 3
+        assert dll.head.data == 3
+        assert dll.tail.data == 1
+        assert dll.head.next.prev == dll.head
+        assert dll.tail.prev.next == dll.tail
+        assert dll.tail.next is None
+        assert dll.head.prev is None 
+        assert dll.size == 3
+
+    def test_delete(self):
+        dll = DoubleLinkedList(array)
+        item1 = 1
+        item2 = 4
+        item3 = 6
+
+        current = dll.head
+
+        #delete first item
+        if dll.head.data == item1:
+            dll.head = dll.head.next
+            dll.head.prev = None
+
+        #delete last item
+        if dll.tail.data == item3:
+            dll.tail = dll.tail.prev
+            dll.tail.next = None
+
+        #delete item in the middle of dll
+        while current is not None:
+           
+            if current.data == item2:
+                current.prev.next = current.next
+                current.next.prev = current.prev
+            current = current.next
+                
+        assert dll.head.data == 2
+        assert dll.tail.data == 5
+        assert dll.head.next.next.data == 5
+        assert dll.tail.prev.data == 3
+
+    def test_delete_implementation(self):
+        dll = DoubleLinkedList(array)
+        assert dll.delete(1) == 1
+        assert dll.delete(4) == 4
+        assert dll.delete(6) == 6
+        assert dll.size == 3
+        assert dll.head.data == 2
+        assert dll.tail.data == 5
+        assert dll.tail.prev.data == 3
+        assert dll.head.next.data == 3
+    
+    def test_find(self):
+        dll = DoubleLinkedList(array)
+        assert dll.find(lambda item: item == 1) == 1
+        assert dll.find(lambda item: item > 1) == 2
+        assert dll.find(lambda item: item < 1) is None
+        assert dll.find(lambda item: item == 10) is None
+    
+    def test_replace(self):
+        dll = DoubleLinkedList(array)
+        item1 = 5
+        item2 = 10
+        item3 = 45
+        assert dll.replace(item1,1) == item1
+        assert dll.replace(item2,4) == item2
+        assert dll.replace(item3,6) == item3
+        assert dll.replace(34,90) is None
+
+        
+       
+                
+
+
+
+
+                
+                
 
 
 
--- double_linked_list.py
+++ double_linked_list.py
@@ -24,5 +24,67 @@ class DoubleLinkedList(object):
             self.size += 1
             return item
         return None
-            
+    
+    def prepend(self,item):
+        new_node = Node(item)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.size += 1
+            return item
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+            self.size += 1
+            return item
+        return None
+
+    def delete(self, item):
+        #delete head
+        if self.head.data == item:
+            self.head = self.head.next
+            self.head.prev = None
+            self.size -= 1
+            return item
+        
+        #delete tail
+        elif self.tail.data == item:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            self.size -= 1
+            return item
+        #delete item in the middle
+        else:
+            current = self.head
+            while current is not None:
+                if current.data == item:
+                    current.prev.next = current.next
+                    current.next.prev = current.prev
+                    self.size -= 1
+                    return item
+                current = current.next
+        return None
+
+    def find(self,quality):
+        current_node = self.head
+        while current_node is not None:
+            if quality(current_node.data):
+                return current_node.data
+            current_node = current_node.next
+        return None
+
+    def replace(self,new_item,old_item):
+        current_node = self.head
+        while current_node is not None:
+            if current_node.data == old_item:
+                current_node.data = None
+                current_node.data = new_item
+                return new_item
+            current_node = current_node.next
+        return None
+        
+
+
+                    
 
